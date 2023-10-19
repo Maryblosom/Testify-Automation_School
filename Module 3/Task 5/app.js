60dd1238db82d4a64751cfc0d04e2447f18b526a
MaryBlossom Onyeka <maryblossom@Eronmonsele.local>
2023-10-19 16:37:30 +0100

    Task 5

---
+++ Module 3/Task 5/app.js
@@ -0,0 +1,31 @@
+const day = "Thursday"
+
+const position = 4
+
+const output = "Today is " + day +". "+"It is the "+ position +"th "+"day of the week."
+
+console.log(output)
+
+// const age = 20
+// const name = 'Maryblossom'
+// const sentence = 'My name is ' + name +'. '+'I am '+age+' years old'
+
+// console.log(sentence)
+
+
+// const myFavLanguage = 'JavaScript'
+
+// console.log('My Favourite language is '+myFavLanguage)
+
+
+// const myAge = 30
+
+// // console.log(myAge + 20)
+
+// const multiplyAge = myAge * 2
+
+// console.log(multiplyAge)
+
+
+
+ 
