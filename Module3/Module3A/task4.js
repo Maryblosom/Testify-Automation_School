59ab2a1b978f5724e35b18fc5ae223344fe08115
MaryBlossom Onyeka <maryblossom@Eronmonsele.local>
2023-10-21 20:56:56 +0100

    creating a new file for task2

--- Module3/Module3A/task4.js
+++ Module3/Module3A/task4.js
@@ -2,16 +2,3 @@
 
 console.log('Hello World')
 
-// Task 3
-/*Fun Facts about JavaScript
-
-1. JavaScript are the de facto standands for building websites
-and websites essentially for vitual orgainisations 
-2. JavaScript is commonly taught for beginners and easy to learn 
-3. While writing JavaScript, you can use semi-colon at the  beginning of a line
-4. In JavaScript, you can Concatenating a number without parsing it.
-5. JavaScript is the world most popular  programming language.
-6. JavaScript is not related to Java contrary to what it's name 
-suggests. 
-7. JavaScripts supports first-class functions. In JavaScript, functions are treated as first-class citizens,
-which means they can be assigned to variables, passed as arguments to other functions, and returned from functions just like any other data type. */
