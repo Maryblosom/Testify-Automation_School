6661b1b0f7b2ab9acabe83523689de0efd720798
MaryBlossom Onyeka <maryblossom@Eronmonsele.local>
2023-10-24 23:36:15 +0100

    adding task7

---
+++ Module3/Module3A/task7.js
@@ -0,0 +1,23 @@
+
+const side1 = 3
+
+const side2 = 2
+
+const side3 = 0
+
+if (side1 && side2 && side3 === 3 ){
+    console.log("Equilateral triangle")
+} else if( side1 || side2 || side3 === 2){
+    console.log("Isosceles triangle")
+} else{
+    console.log("Scalene triangle")
+}
+          
+
+
+
+
+
+
+
+
