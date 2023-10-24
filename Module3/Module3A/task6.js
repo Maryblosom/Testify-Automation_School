80b23293c2c0f04e5d05aa6a44a4f0a841ea873f
MaryBlossom Onyeka <maryblossom@Eronmonsele.local>
2023-10-24 08:00:27 +0100

    Add Task 6

---
+++ Module3/Module3A/task6.js
@@ -0,0 +1,7 @@
+const number = 33
+
+if (number % 2){
+    console.log(number+ ' is odd')
+}else {
+    console.log(number+ ' is even')
+}
---
+++ Module3/app.js
@@ -0,0 +1,30 @@
+
+const age = 78
+
+if(age>=18 && age <= 65) {
+    console.log('You are eligiable to vote')
+} else if(age>65){
+    console.log('You are too old to vote')
+} else {
+    console.log('You are too young to vote')
+}
+
+
+
+// if(age>=18) {
+//     console.log('You are eligiable to vote')
+// } else if(age>65){
+//     console.log('You are too old to vote')
+// } else {
+//     console.log('You are too young to vote')
+// }
+
+
+/*if(age>=18) {
+    console.log('You are eligable to vote')*/
+
+// if(age>=18) {
+//     console.log('You are eligable to vote')
+// } else{
+//     console.log('You are too young to vote')
+// }
