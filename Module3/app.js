d5d3e455e38f1369aaae1ed7cdbadf902cc8bf40
MaryBlossom Onyeka <maryblossom@Eronmonsele.local>
2023-10-26 08:32:46 +0100

    commit task 8

--- Module3/app.js
+++ Module3/app.js
@@ -1,13 +1,72 @@
-
-const age = 78
-
-if(age>=18 && age <= 65) {
-    console.log('You are eligiable to vote')
-} else if(age>65){
-    console.log('You are too old to vote')
-} else {
-    console.log('You are too young to vote')
+for(let number = 1; number <=10; number = number +1){
+    if(number % 2 === 0) {
+        console.log(number +" is an even number")
+    } else {
+        console.log(number +" is an odd number")
+    }
 }
+
+//       //While Loops
+   
+// let star = 1 //starting point 
+//  while(star<=100){ //condition
+//     if(star === 1){
+//         console.log(star + " star")
+//     } else
+//         console.log(star + " stars")
+     
+//      star = star + 1 // statment
+//  }
+
+
+
+
+// const day = "Thursday"
+
+// if(day==="Friday"){
+//     console.log("TGIF")
+// } else if("Saturday"){
+//     console.log("Yeh! The weekend is here")
+// } else if("Sunday"){
+//     console.log("Happy Sunday")
+// } else {
+//     console.log("Go to work!")
+// } instead of running all these, use the switch statment(conditional statement) 
+
+
+// switch(day){
+//     case "Friday":
+//         console.log("TGIF")
+//         break
+//     case "Saturday":
+//         console.log("Yeh! It is weekend")
+//         break
+//     case "Sunday":
+//         console.log("Happy Sunday")
+//         break
+//     default:
+//         console.log("Go to work!")        
+        
+
+// }
+
+
+
+
+
+
+
+
+
+// const age = 78
+
+// if(age>=18 && age <= 65) {
+//     console.log('You are eligiable to vote')
+// } else if(age>65){
+//     console.log('You are too old to vote')
+// } else {
+//     console.log('You are too young to vote')
+// }
 
 
 
