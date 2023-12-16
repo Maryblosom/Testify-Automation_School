1c515ddb3b7eca715a8c39650891cf5360023cf7
MaryBlossom Onyeka <maryblossom@Eronmonsele.local>
2023-12-16 18:46:37 +0100

    add tasks

--- Module4/Module4A/Tasks/Task12/ClassA.java
+++ Module4/Module4A/Tasks/Task12/ClassA.java
@@ -3,7 +3,7 @@ package com.Graham.Task12;
 public class ClassA {
     public static void main(String[] args) {
         AccessClassA access = new AccessClassA();
-        access.accessBMethod();
+        access.accessAMethod();
     }
 
 }
